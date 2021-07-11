369447fc7af97f506a64c40e65f5840d7eb05fed
dukwar <d.clopow@yandex.ru>
2021-07-12 01:02:13 +0300

    added background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import {start} from "./game/main";
 
 function App() {
+
+    useEffect(() => {
+        start()
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <>
+        <canvas id="screen" width={1000} height={1000}>
+
+        </canvas>
+      </>
+
+
   );
 }
 
